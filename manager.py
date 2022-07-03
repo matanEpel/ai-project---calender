6206a6fdd4a35dc322614bcf6b8871caa75c4bd6
matanepel <matan.epel12@gmail.com>
2022-07-03 12:48:28 +0300

    writing manager code

--- manager.py
+++ manager.py
@@ -1,3 +1,20 @@
+from user import User
+
+
 class Manager:
     def __init__(self):
+        self.__users = []
+
+    def get_users(self):
+        return self.__users
+
+    def add_user(self, user: User):
+        self.__users.append(user)
+
+    def del_user(self, user: User):
+        if user in self.__users:
+            self.__users.remove(user)
+
+    def schedule_week(self, week):
+        # TODO: implement this using forward checking
         pass

8c71cecd318b42e8e73f7a7602c7204d51d91bb3
matanepel <matan.epel12@gmail.com>
2022-07-03 10:37:16 +0300

    initial commit

---
+++ user.py
@@ -0,0 +1,12 @@
+class user:
+    def __init__(self):
+        pass
+
+    def add_assignment(self, week, length, name):
+        pass
+
+    def get_all_assignments(self, week):
+        pass
+
+    def get_schedule(self):
+        pass

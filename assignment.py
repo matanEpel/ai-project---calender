dabc8ad6dc823f919defd3939a9fa80adc7aa48e
matanepel <matan.epel12@gmail.com>
2022-07-03 11:29:05 +0300

    starting to write some classes

--- assignment.py
+++ assignment.py
@@ -1,5 +1,15 @@
 from consts import *
-from user import *
+from user import User
+
+
+# TODO: implement language processing model
+def generate_kind(name):
+    """
+    Generates a kind based on language processing model.
+    Sets the kind to the output of the model.
+    :return: none
+    """
+    return kinds["TASK"]
 
 
 class Assignment:
@@ -7,27 +17,22 @@ class Assignment:
     The basic Assignment class.
     The class defines all the properties of an assignment - week, name,
     kind, day and hour.
-    All the stuff here is pretty straight forward except for the generate_kind
+    All the stuff here is pretty straight forward except for the use of generate_kind
     function which is the "AI" part of this class
     """
 
     def __init__(self, week, name, participants=None, kind=None, day=None, hour=None):
         if participants is None:
             participants = []
+        if not kind:
+            # if kind was not given - we generate it based on the assignment name
+            kind = generate_kind(name)
         self.__week = week
         self.__name = name
         self.__kind = kind
         self.__day = day
         self.__hour = hour
         self.__participants = participants
-
-    def generate_kind(self):
-        """
-        Generates a kind based on language processing model.
-        Sets the kind to the output of the model.
-        :return: none
-        """
-        self.__kind = kinds["TASK"]
 
     def get_week(self):
         return self.__week
@@ -49,23 +54,27 @@ class Assignment:
 
     def add_participant(self, user: User):
         self.__participants.append(user)
+
+        # changes the kind to meeting because there are participants now:
         self.set_kind(kinds["MEETING"])
 
     def set_participants(self, participants):
         self.__participants = participants
+
+        # changes the kind to meeting because there are participants now:
         self.set_kind(kinds["MEETING"])
 
-    def set_week(self, week):
+    def set_week(self, week: int):
         self.__week = week
 
-    def set_name(self, name):
+    def set_name(self, name: int):
         self.__name = name
 
-    def set_kind(self, kind):
+    def set_kind(self, kind: int):
         self.__kind = kind
 
-    def set_day(self, day):
+    def set_day(self, day: int):
         self.__day = day
 
-    def set_hour(self, hour):
+    def set_hour(self, hour: int):
         self.__hour = hour
--- user.py
+++ user.py
@@ -2,14 +2,54 @@ from assignment import *
 
 
 class User:
-    def __init__(self):
-        pass
+    """
+    The user class. Every user include its name, and all of his tasks for
+    every week. Additionally, it includes the schedule for all the tasks
+    of of each week.
+    """
+    def __init__(self, name: str):
+        self.__name = name
+        self.__assignments = dict()
+
+        # schedule is a dict of assignments for every week that must include an hour attached to them
+        self.__schedule = dict()
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
 
     def add_assignment(self, assignment: Assignment):
-        pass
+        self.__assignments[assignment.get_week()].append(assignment)
 
-    def get_all_assignments(self, week: int):
-        pass
+    def remove_assignment(self, assignment: Assignment):
+        """
+        removes an assignment from the assignmets of the user
+        :param assignment: name of the assignment
+        :return: none
+        """
+        week = assignment.get_week()
+        if week in self.__assignments and assignment in self.__assignments[week]:
+            self.__assignments[week].remove(assignment)
+        if week in self.__schedule and assignment in self.__schedule[week]:
+            self.__schedule[week].remove(assignment)
 
-    def get_schedule(self):
-        pass
+        # schedule must be changed if we modified our assignments in order to be optimal
+        del self.__schedule[week]
+
+    def get_assignments(self, week: int):
+        return self.__assignments[week]
+
+    def get_schedule(self, week: int):
+        return self.__schedule[week]
+
+    def set_schedule(self, week: int):
+        """
+        This function copies all the assignment of a week into the schedule dict
+        It basically means that the manager has modified the hours and days of every assignment
+        so it is now in a scheduled form
+        :param week: the week that was scheduled
+        :return: none
+        """
+        self.__schedule[week] = self.__assignments[week]

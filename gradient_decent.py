9e520b5be928fb69ac58c840e1ba2c30f0f81918
matanepel <matan.epel12@gmail.com>
2022-07-04 15:33:43 +0300

    writing gradient decent code

--- gradient_decent.py
+++ gradient_decent.py
@@ -30,3 +30,30 @@ class GradientDecent:
             time_slots = [self.__free_times[k]["free slots"] for k in self.__meetings[i]["participants"]]
             optional_slots[i] += find_possible_slots(self.__meetings[i]["duration"], time_slots)
 
+        time_for_each_meeting = self.get_random_start_point(optional_slots) # [(day, time) - list of times for the meetings]
+        for i in range(len(self.__meetings)):
+            self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
+            self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+        scores = [user.schedule_week(self.__week) for user in self.__users]
+        prev_score = -np.inf
+        new_score = self.score(scores)
+        while new_score > prev_score:
+            max = -np.inf
+            new_times = []
+            for new_times_for_each_meeting in self.get_neighbors_time(optional_slots, time_for_each_meeting):
+                for i in range(len(self.__meetings)):
+                    self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
+                    self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+                scores = [user.schedule_week(self.__week) for user in self.__users]
+                if self.score(scores) > max:
+                    max = self.score(scores)
+                    new_times = new_times_for_each_meeting
+            prev_score = new_score
+            new_score = max
+            if new_score > prev_score:
+                time_for_each_meeting = new_times
+        for i in range(len(self.__meetings)):
+            self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
+            self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+
+

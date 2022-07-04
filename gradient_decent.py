1c26c34461418964ae89e54914bfc30a4f56b9a3
matanepel <matan.epel12@gmail.com>
2022-07-04 16:32:57 +0300

    writing gradient decent code

--- gradient_decent.py
+++ gradient_decent.py
@@ -1,5 +1,8 @@
+import random
+
 import numpy as np
 
+from time_ import Time
 from time_slots import find_possible_slots
 
 
@@ -25,35 +28,86 @@ class GradientDecent:
             return -np.std(np.array(scores))
 
     def solve(self):
+        # get all possible slots for each meeting:
         optional_slots = [[] for _ in range(len(self.__meetings))] # optional slots for every meeting
         for i in self.__meetings.keys():
             time_slots = [self.__free_times[k]["free slots"] for k in self.__meetings[i]["participants"]]
             optional_slots[i] += find_possible_slots(self.__meetings[i]["duration"], time_slots)
 
-        time_for_each_meeting = self.get_random_start_point(optional_slots) # [(day, time) - list of times for the meetings]
+        final_time_for_each_meeting = []
+        final_score = -np.inf
+
+        # trying X different starting points:
+        for _ in range(100):
+            # get random starting point:
+            time_for_each_meeting = self.get_random_start_point(optional_slots) # [(day, time) - list of times for the meetings]
+            for i in range(len(self.__meetings)):
+                self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
+                self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+            scores = [user.schedule_week(self.__week) for user in self.__users]
+            prev_score = -np.inf
+            new_score = self.score(scores)
+            # doing the gradient decent part
+            while new_score > prev_score:
+                max = -np.inf
+                new_times = []
+                for new_times_for_each_meeting in self.get_neighbor_times(optional_slots, time_for_each_meeting):
+                    for i in range(len(self.__meetings)):
+                        self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
+                        self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+                    scores = [user.schedule_week(self.__week) for user in self.__users]
+                    if self.score(scores) > max:
+                        max = self.score(scores)
+                        new_times = new_times_for_each_meeting
+                prev_score = new_score
+                new_score = max
+                if new_score > prev_score:
+                    time_for_each_meeting = new_times
+            if prev_score > final_score:
+                final_score = prev_score
+                final_time_for_each_meeting = time_for_each_meeting
+
         for i in range(len(self.__meetings)):
-            self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
-            self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
-        scores = [user.schedule_week(self.__week) for user in self.__users]
-        prev_score = -np.inf
-        new_score = self.score(scores)
-        while new_score > prev_score:
-            max = -np.inf
-            new_times = []
-            for new_times_for_each_meeting in self.get_neighbors_time(optional_slots, time_for_each_meeting):
-                for i in range(len(self.__meetings)):
-                    self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
-                    self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
-                scores = [user.schedule_week(self.__week) for user in self.__users]
-                if self.score(scores) > max:
-                    max = self.score(scores)
-                    new_times = new_times_for_each_meeting
-            prev_score = new_score
-            new_score = max
-            if new_score > prev_score:
-                time_for_each_meeting = new_times
-        for i in range(len(self.__meetings)):
-            self.__meetings[i]["object"].set_day(time_for_each_meeting[0])
-            self.__meetings[i]["object"].set_time(time_for_each_meeting[1])
+            self.__meetings[i]["object"].set_day(final_time_for_each_meeting[0])
+            self.__meetings[i]["object"].set_time(final_time_for_each_meeting[1])
+
+    def get_random_start_point(self, optional_slots):
+        slots = []
+        for slots_of_day in optional_slots:
+            available_slots = []
+            for curr_slot in slots_of_day:
+                available = True
+                for slot in slots:
+                    if curr_slot["day"] == slot["day"] and Time.is_overlap(curr_slot["start"], curr_slot["finish"], slot["start"], slot["finish"]):
+                        available = False
+                if available:
+                    available_slots.append(curr_slot)
+            slot = random.choice(slots_of_day)
+            slots.append(slot)
+
+        return [(s["day"], s["start"]) for s in slots]
+
+    def get_neighbor_times(self, optional_slots, time_for_each_meeting):
+        """
+        the neighbors are all the optional switches between the slots
+        :param optional_slots:
+        :param time_for_each_meeting:
+        :return:
+        """
+        all_options = []
+        for i in range(len(time_for_each_meeting)):
+            for j in range(i+1,len(time_for_each_meeting)):
+                new_option = time_for_each_meeting[:i] + [time_for_each_meeting[j]] + time_for_each_meeting[i+1:j] + [time_for_each_meeting[i]] + time_for_each_meeting[j+1:]
+                if self.is_consistent(new_option, optional_slots):
+                    all_options.append(new_option)
+        return all_options
+
+    def is_consistent(self, option, optional_slots):
+        for i in range(len(option)):
+            time = option[i]
+            if all([time["start"] != slot["start"] for slot in optional_slots[i] if slot["day"] == time["day"]]):
+                return False
+        return True
+
 
 
